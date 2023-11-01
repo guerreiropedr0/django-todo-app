43bf04ad26f958a74c5b2e7bd1ad6e73b0cf1825
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2023-11-01 12:21:31 +0000

    Add due_date field to tasks

--- todos/models.py
+++ todos/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Task(models.Model):
   description = models.CharField(max_length=200)
   is_completed = models.BooleanField(default=False)
+  due_date = models.DateField(null=True, blank=True)
 
   def __str__(self):
     return self.description
